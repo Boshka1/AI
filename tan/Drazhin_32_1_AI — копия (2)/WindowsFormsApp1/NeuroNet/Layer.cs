d3816059fcd98d9579fef0a7d59675c4ede607df
Boshka1 <edukub112@gmail.com>
2025-11-24 23:18:28 +0300

    Add files via upload

---
+++ tan/Drazhin_32_1_AI — копия (2)/WindowsFormsApp1/NeuroNet/Layer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Drazhin_32_1_AI.NeuroNet;
+
+namespace WindowsFormsApp1.NeuroNet
+{
+    abstract class Layer
+    {
+        //Поля
+        protected string name_Layer; //название слоя
+        string pathDirWeights; //путь к каталогу, где находится файл синаптических весов
+        string pathFileWeights; //путь к файлу саниптическов весов
+        protected int numofneurons; //число нейронов текущего слоя
+        protected int numofprevneurons; //число нейронов предыдущего слоя
+        protected const double learningrate = 0.065; //скорость обучения 0.03 037 03
+        protected const double momentum = 0.1d; //момент инерции 0.2  004 08я
+        protected double[,] lastdeltaweights; //веса предыдущей итерации
+        protected Neuron[] neurons; //массив нейронов текущего слоя
+
+        //Свойства
+        public Neuron[] Neurons { get => neurons; set => neurons = value; }
+        public double[] Data //Передача входных сигналов на нейроны слоя и авктиватор
+        {
+            set
+            {
+                for (int i = 0; i < numofneurons; i++)
+                {
+                    Neurons[i].Activator(value);
+                }
+            }
+        }
+
+        //Конструктор
+        protected Layer(int non, int nopn, NeuronType nt, string nm_Layer)
+        {
+            numofneurons = non; //количество нейронов текущего слоя
+            numofprevneurons = nopn; //количество нейронов предыдущего слоя
+            Neurons = new Neuron[non]; //определение массива нейронов
+            name_Layer = nm_Layer; //наиминование слоя
+            pathDirWeights = AppDomain.CurrentDomain.BaseDirectory + "memory\\";
+            pathFileWeights = pathDirWeights + name_Layer + "_memory.csv";
+
+            double[,] Weights; //временный массив синаптических весов
+            lastdeltaweights = new double[non, nopn + 1];
+
+            if (File.Exists(pathFileWeights)) //определяет существует ли pathFileWeights
+                Weights = WeightInitialize(MemoryMode.GET, pathFileWeights); //считывает данные из файла
+            else
+            {
+                Directory.CreateDirectory(pathDirWeights);
+                Weights = WeightInitialize(MemoryMode.INIT, pathFileWeights);
+            }
+
+            for (int i = 0; i < non; i++) //цикл формирования нейронов слоя и заполнения
+            {
+                double[] tmp_weights = new double[nopn + 1];
+                for (int j = 0; j < nopn; j++)
+                {
+                    tmp_weights[j] = Weights[i, j];
+                }
+                Neurons[i] = new Neuron(tmp_weights, nt); //заполнение массива нейронами
+            }
+        }
+
+
+        //Метод работы с массивом синаптических весов слоя
+        public double[,] WeightInitialize(MemoryMode mm, string path)
+        {
+            char[] delim = new char[] { ';', ' ' };
+            string tmpStr;
+            string[] tmpStrWeights;
+            double[,] weights = new double[numofneurons, numofprevneurons + 1];
+
+
+            switch (mm)
+            {
+                case MemoryMode.GET:
+                    tmpStrWeights = File.ReadAllLines(path); //читаем все строки файла
+                    string[] memory_element; //временный массив, хранящий веса одного нейрона в виде строк
+                    for (int i = 0; i < numofneurons; i++)
+                    {
+                        memory_element = tmpStrWeights[i].Split(delim);
+                        for (int j = 0; j < numofprevneurons + 1; j++)
+                        {
+                            weights[i, j] = double.Parse(memory_element[j].Replace(',', '.'),
+                                System.Globalization.CultureInfo.InvariantCulture);
+                        }
+                    }
+                    break;
+
+
+                case MemoryMode.SET:
+                    tmpStr = "";
+                    for (int i = 0; i < numofneurons; i++)
+                    {
+                        string[] tmpRow = new string[numofprevneurons + 1];
+                        for (int j = 0; j < numofprevneurons + 1; j++)
+                        {
+                            tmpRow[j] = this.neurons[i].Weights[j].ToString(System.Globalization.CultureInfo.InvariantCulture);
+                        }
+                        tmpStr += string.Join(";", tmpRow) + "\n";
+                    }
+                    File.WriteAllText(path, tmpStr);
+                    break;
+
+
+                case MemoryMode.INIT:
+                    Random random = new Random();
+                    for (int i = 0; i < numofneurons; i++)
+                    {
+                        double sum = 0.0;
+                        double squaredSum = 0.0;
+
+                        //Генерация весов
+                        for (int j = 0; j < numofprevneurons + 1; j++)
+                        {
+                            weights[i, j] = random.NextDouble() * 2.0 - 1.0;
+                            sum += weights[i, j];
+                            squaredSum += weights[i, j] * weights[i, j];
+                        }
+
+                        //Вычисляем среднее и дисперсию
+                        double mean = sum / (numofprevneurons + 1);
+                        double variance = (squaredSum / (numofprevneurons + 1)) - (mean * mean);
+                        double root = Math.Sqrt(variance);
+
+                        //Нормализуем веса
+                        for (int j = 0; j < numofprevneurons + 1; j++)
+                        {
+                            if (numofprevneurons > 0)
+                                weights[i, j] = (weights[i, j] - mean) / root / Math.Sqrt(numofprevneurons);
+                            else
+                                weights[i, j] = (weights[i, j] - mean) / root;
+                        }
+                    }
+
+                    //Сохранение весов
+                    tmpStr = "";
+                    for (int i = 0; i < numofneurons; i++)
+                    {
+                        string[] tmpRow = new string[numofprevneurons + 1];
+                        for (int j = 0; j < numofprevneurons + 1; j++)
+                        {
+                            tmpRow[j] = weights[i, j].ToString(System.Globalization.CultureInfo.InvariantCulture);
+                        }
+                        tmpStr += string.Join(";", tmpRow) + "\n";
+                    }
+                    File.WriteAllText(path, tmpStr);
+                    break;
+            }
+            return weights;
+        }
+        abstract public void Recognize(Network net, Layer nextLayer); //для прямых проходов
+        abstract public double[] BackwardPass(double[] stuff); //и обратных
+    }
+}
---
+++ tan/Drazhin_32_1_AI — копия (2)/WindowsFormsApp1/NeuroNet/Neuron.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using static System.Math;
+
+namespace Drazhin_32_1_AI.NeuroNet
+{
+    class Neuron
+    {
+        // поля
+        private NeuronType type; //тип нейрона
+        private double[] weights; //его веса
+        private double[] inputs; //его входы
+        private double output;
+        private double derivative;
+
+        //константы для функц активации
+        private double a = 0.01d;
+
+        //свойства
+        public double[] Weights { get => weights; set => weights = value; }
+        public double[] Inputs { get => inputs; set => inputs = value; }
+        public double Output { get => output; }
+        public double Derivative { get => derivative; }
+
+        //конструктор
+        public Neuron(double[] memoryWeights, NeuronType typeNeuron)
+        {
+            type = typeNeuron;
+            weights = memoryWeights;
+        }
+        public static double Tanh(double x)
+        {
+            return Math.Tanh(x);
+        }
+        public static double TanhDerivative(double x)
+        {
+            double tanh = Math.Tanh(x);
+            return 1 - tanh * tanh;
+        }
+        public void Activator(double[] i)
+        {
+            {
+                inputs = i;
+                double sum = weights[0];
+                for (int j = 0; j < inputs.Length; j++)
+                {
+                    sum += inputs[j] * weights[j + 1];
+                }
+
+                switch (type)
+                {
+                    case NeuronType.Hidden:
+                        // Сначала считаем выход
+                        output = Math.Tanh(sum);
+                        // Производная Tanh(x) = 1 - Tanh(x)^2
+                        // Мы уже знаем Tanh(x) (это output), поэтому просто используем его.
+                        derivative = 1.0d - output * output;
+                        break;
+                    case NeuronType.Output:
+                        output = Math.Exp(sum);
+                        // Для Softmax+CrossEntropy производная здесь не нужна в явном виде,
+                        // она сокращается в OutputLayer.cs
+                        break;
+                }
+            }
+        }
+    }
+
+}
